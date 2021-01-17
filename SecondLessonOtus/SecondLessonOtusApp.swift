2f19dd640c7b0d648005715ce6948710e7513694
Vlad Kalaev <kalaevwlad@gmail.com>
2021-01-17 16:36:28 +0300

    Initial Commit

---
+++ SecondLessonOtus/SecondLessonOtusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondLessonOtusApp.swift
+//  SecondLessonOtus
+//
+//  Created by Влад Калаев on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SecondLessonOtusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
